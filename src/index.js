ceef985b14debe5735148599cea0ad2a52d2b161
Bob Whitelock <bob.whitelock1@gmail.com>
2018-08-02 13:17:39 +0100

    Remove initial generated CSS and image

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-import './main.css';
-import { Main } from './Main.elm';
+import {Main} from './Main.elm';
 import registerServiceWorker from './registerServiceWorker';
 
 Main.embed(document.getElementById('root'));
