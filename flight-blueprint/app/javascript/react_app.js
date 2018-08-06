116de7edbfdb22c358ee90c92e87c241b125e964
Bob Whitelock <bob.whitelock1@gmail.com>
2018-08-06 16:09:11 +0100

    Have original Elm app rendered within Rails app

--- flight-blueprint/app/javascript/react_app.js
+++ flight-blueprint/app/javascript/react_app.js
@@ -1,16 +1,29 @@
 import React from 'react';
-import Elm from 'react-elm-components';
+import ReactElmComponent from 'react-elm-components';
+import yaml from 'js-yaml';
 
 import {Main as ElmApp} from 'Main';
 
 const Header = props => <div>Header</div>;
 const Footer = props => <div>Footer</div>;
 
+const ElmAppComponent = props => {
+  const setupPorts = ports => {
+    const convertToYaml = object => {
+      const yamlString = yaml.safeDump(object);
+      ports.convertedYaml.send(yamlString);
+    };
+    ports.convertToYaml.subscribe(convertToYaml);
+  };
+
+  return <ReactElmComponent src={ElmApp} ports={setupPorts} />;
+};
+
 const App = props => {
   return (
     <div>
       <Header />
-      <Elm src={ElmApp} />
+      <ElmAppComponent />
       <Footer />
     </div>
   );
