bfa4e0aebda52899d612b71197603ad4ec0bed15
Ben Armston <ben.armston@alces-software.com>
2018-09-05 10:56:09 +0100

    Add status route for uptime monitor

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Ensure there is a simple route for uptime-robot to request.
+  get '/status' => (lambda do |req|
+    [200, {}, ["OK"]]
+  end)
 end
